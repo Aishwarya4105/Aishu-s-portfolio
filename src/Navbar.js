d43cc032c7d1081ee2c49e503e5f02f80ef68c18
Aishwarya <aishwarya4102025@gmail.com>
2025-11-29 22:22:20 +0530

    media query

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+
 
 export default function Navbar() {
+  const [open,setOpen]=useState(false);
   return (
-    <nav className="navbar">
+
+    <nav className="navbar" >
       <h2 className="logo">Aishwarya</h2>
-      <ul className="nav-links">
+      <ul className={open ? "nav-links open" :"nav-links"}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/About">About</Link></li>
         <li><Link to="/Education">Education</Link></li>
         <li><Link to="/Contact">Contact</Link></li>
       </ul>
+    
+    <div className="hamburger" onClick={()=> setOpen(!open)}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
     </nav>
   );
 }
